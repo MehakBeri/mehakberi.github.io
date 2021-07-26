137a5e5701a720a1ade948594d0780a39582a261
MehakBeri <mehakberi1996@gmail.com>
2021-07-25 23:39:37 -0500

    menu cleanup

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
@@ -9,7 +9,24 @@ export class PortfolioComponent implements OnInit {
 
   constructor() { }
 
+  @ViewChildren('project' ) p: ElementRef;
+
   ngOnInit() {
+  }
+
+  filter(tag: string) {
+    console.log(tag);
+    if(this.p) {
+      this.p['_results'].forEach(element => {
+        console.log(element.nativeElement.innerHTML)
+        if (element.nativeElement.innerHTML.indexOf(tag)==-1) {
+          console.log('hiding')
+          element.nativeElement.style.display='none';
+        } else {
+          element.nativeElement.style.display='block';
+        }
+      });
+    } 
   }
 
 }
